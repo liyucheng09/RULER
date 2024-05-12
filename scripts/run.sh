bc2c92488180bf3cd40b04f710af57540e9d32a0
Yucheng Li <liyucheng09@gmail.com>
2024-05-12 04:08:43 +0000

    max_model_len

--- scripts/run.sh
+++ scripts/run.sh
@@ -64,6 +64,7 @@ if [ "$MODEL_FRAMEWORK" == "vllm" ]; then
         --tensor-parallel-size=${GPUS} \
         --dtype bfloat16 \
         --disable-custom-all-reduce \
+        --max-model-len 140000 \
         &
 
 elif [ "$MODEL_FRAMEWORK" == "trtllm" ]; then
